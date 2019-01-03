a3b14bafa2cae163f4f7fb585740a87d76696d63
h1-the-swan <jason.portenoy@gmail.com>
2019-01-02 21:31:56 -0800

    add headshot

--- src/index.js
+++ src/index.js
@@ -19,15 +19,25 @@ var projects = [
 			Visualizing Scholarly Influence <a href="http://scholar.eigenfactor.org/" target="_blank">Link</a>
 			`
 	},
+	{
+		"name": "Predictors of Permanent Housing for Homeless Families",
+		"img": require('./assets/images/sankey_thumb.png'),
+		"description": 
+			`
+			<a href="http://escience.washington.edu/research-project/predictors-of-permanent-housing-for-homeless-families/" target="_blank">Link</a>
+			`
+	},
 ];
 
 
 
 projects.forEach(function(item) {
-	var p = $('#project-item-template');
+	var p = $('#project-item-template').clone();
 	p.find('img').attr('src', item.img);
 	p.find('.project-name').html(item.name);
 	p.find('.project-description').html(item.description);
 	$('#projects').append(p.show());
 });
 
+$('#headshot').find('img').attr('src', require('./assets/images/jason_headshot_ischool_11-9-2018_1-748_lo.jpg'));
+console.log($('#headshot'));
